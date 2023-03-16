324756e59c018fa02fe77e21838bd6a2ca4da673
amyrold <66313537+amyrold@users.noreply.github.com>
2023-03-16 12:48:58 -0500

    commenting code block

--- PipelineProject_aaron_myrold/PipelineProject.py
+++ PipelineProject_aaron_myrold/PipelineProject.py
@@ -73,41 +73,41 @@ if raw_or_test == 'test':
 
 
 
-# PART 2 ----
-# Here I download the reference genome (HCMV) in order to build the index for bowtie2
-# I use the Entrez.efetch() method to download the viral genome
-# Then I run a bowtie2 command to build the index inside of the 4_index folder
-os.chdir(p_index)
-Entrez.email = "amyrold@luc.edu"
-# code adapted from https://www.biostars.org/p/261774/
-# code originally from biopython cookbook, wasn't sure which to link so I linked the site I found it from.
-filename = "NC_006273.2.fasta"
-if not os.path.isfile(filename): # To prevent overwriting the file
-    net_handle = Entrez.efetch(
-        db="nucleotide", id="NC_006273.2", rettype="fasta", retmode="text"
-    )
-    out_handle = open(filename, "w")
-    out_handle.write(net_handle.read())
-    out_handle.close()
-    net_handle.close()
-record = SeqIO.read(filename, "fasta") # open file as fasta - i dont think this is needed
-os.system('bowtie2-build NC_006273.2.fasta HCMV')
-os.chdir('..')
+# # PART 2 ----
+# # Here I download the reference genome (HCMV) in order to build the index for bowtie2
+# # I use the Entrez.efetch() method to download the viral genome
+# # Then I run a bowtie2 command to build the index inside of the 4_index folder
+# os.chdir(p_index)
+# Entrez.email = "amyrold@luc.edu"
+# # code adapted from https://www.biostars.org/p/261774/
+# # code originally from biopython cookbook, wasn't sure which to link so I linked the site I found it from.
+# filename = "NC_006273.2.fasta"
+# if not os.path.isfile(filename): # To prevent overwriting the file
+#     net_handle = Entrez.efetch(
+#         db="nucleotide", id="NC_006273.2", rettype="fasta", retmode="text"
+#     )
+#     out_handle = open(filename, "w")
+#     out_handle.write(net_handle.read())
+#     out_handle.close()
+#     net_handle.close()
+# record = SeqIO.read(filename, "fasta") # open file as fasta - i dont think this is needed
+# os.system('bowtie2-build NC_006273.2.fasta HCMV')
+# os.chdir('..')
 
-# save only the reads that map
-os.system(f'bowtie2 -x {p_index}/HCMV -1 {p_data_raw}/SRR5660030_1.fastq.gz -2 {p_data_raw}/SRR5660030_2.fastq.gz -S {p_data_clean}/HCMVmap_30.sam --al-conc-gz {p_data_clean}/SRR5660030_mapped_%.fq.gz')
-os.system(f'bowtie2 -x {p_index}/HCMV -1 {p_data_raw}/SRR5660033_1.fastq.gz -2 {p_data_raw}/SRR5660033_2.fastq.gz -S {p_data_clean}/HCMVmap_33.sam --al-conc-gz {p_data_clean}/SRR5660033_mapped_%.fq.gz')
-os.system(f'bowtie2 -x {p_index}/HCMV -1 {p_data_raw}/SRR5660044_1.fastq.gz -2 {p_data_raw}/SRR5660044_2.fastq.gz -S {p_data_clean}/HCMVmap_44.sam --al-conc-gz {p_data_clean}/SRR5660044_mapped_%.fq.gz')
-os.system(f'bowtie2 -x {p_index}/HCMV -1 {p_data_raw}/SRR5660045_1.fastq.gz -2 {p_data_raw}/SRR5660045_2.fastq.gz -S {p_data_clean}/HCMVmap_45.sam --al-conc-gz {p_data_clean}/SRR5660045_mapped_%.fq.gz')
+# # save only the reads that map
+# os.system(f'bowtie2 -x {p_index}/HCMV -1 {p_data_raw}/SRR5660030_1.fastq.gz -2 {p_data_raw}/SRR5660030_2.fastq.gz -S {p_data_clean}/HCMVmap_30.sam --al-conc-gz {p_data_clean}/SRR5660030_mapped_%.fq.gz')
+# os.system(f'bowtie2 -x {p_index}/HCMV -1 {p_data_raw}/SRR5660033_1.fastq.gz -2 {p_data_raw}/SRR5660033_2.fastq.gz -S {p_data_clean}/HCMVmap_33.sam --al-conc-gz {p_data_clean}/SRR5660033_mapped_%.fq.gz')
+# os.system(f'bowtie2 -x {p_index}/HCMV -1 {p_data_raw}/SRR5660044_1.fastq.gz -2 {p_data_raw}/SRR5660044_2.fastq.gz -S {p_data_clean}/HCMVmap_44.sam --al-conc-gz {p_data_clean}/SRR5660044_mapped_%.fq.gz')
+# os.system(f'bowtie2 -x {p_index}/HCMV -1 {p_data_raw}/SRR5660045_1.fastq.gz -2 {p_data_raw}/SRR5660045_2.fastq.gz -S {p_data_clean}/HCMVmap_45.sam --al-conc-gz {p_data_clean}/SRR5660045_mapped_%.fq.gz')
 
-# write the number of reads that map before and after bowtie2 to log file
-# for file in raw, count reads
-# forloop and count from: https://www.biostars.org/p/139006/
-os.chdir(p_data_raw)
-os.system('for i in `ls *.fastq.gz`; do echo $(zcat ${i} | wc -l)/4|bc; done')
-# for file in clean, count reads
-os.chdir(p_data_raw)
-os.system('for i in `ls *.fastq.gz`; do echo $(zcat ${i} | wc -l)/4|bc; done')
+# # write the number of reads that map before and after bowtie2 to log file
+# # for file in raw, count reads
+# # forloop and count from: https://www.biostars.org/p/139006/
+# os.chdir(p_data_raw)
+# os.system('for i in `ls *.fastq.gz`; do echo $(zcat ${i} | wc -l)/4|bc; done')
+# # for file in clean, count reads
+# os.chdir(p_data_raw)
+# os.system('for i in `ls *.fastq.gz`; do echo $(zcat ${i} | wc -l)/4|bc; done')
 
 
 # PART 3 ----
